91ddb5813320306f8c39a7563a52a247293f456d
kyooowe <brinasjohnmark@gmail.com>
2023-03-06 23:04:10 +0800

    Changes
    Core
    - Uninstall not needed packages
    
    React
    - Add Formik form
    - Add React-Hook-Form

---
+++ src/snippets/react-snippets/js/forms/formik.jsx
@@ -0,0 +1,65 @@
+//#region Import
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+//#endregion
+
+//#region Validation Schema
+const validationSchema = yup.object({
+	name: yup
+		.string()
+		.required('Product Name is Required'),
+	type: yup.string().required('Product Type is Required')
+});
+
+const ProductsForm = () => {
+
+	//#region Formik
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			type: ''
+		},
+		validationSchema: validationSchema,
+		onSubmit: async (values) => {
+
+			// Return object values in form
+			console.log(values)
+		}
+	});
+	//#endregion
+
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<input
+				name="name"
+				type="text"
+				onChange={formik.handleChange}
+				value={formik.values.name}
+			/>
+			{
+				formik.touched.name && Boolean(formik.errors.name) ? (
+					<span style={{ color: 'red' }}>{formik.errors.name}</span>
+				) : (
+					null
+				)
+			}
+			<input
+				name="type"
+				type="text"
+				onChange={formik.handleChange}
+				value={formik.values.type}
+			/>
+			{
+				formik.touched.type && Boolean(formik.errors.type) ? (
+					<span style={{ color: 'red' }}>{formik.errors.type}</span>
+				) : (
+					null
+				)
+			}
+
+			<button type="submit">Submit</button>
+		</form>
+	)
+}
+
+export default ProductsForm;
---
+++ src/snippets/react-snippets/js/forms/rhform.jsx
@@ -0,0 +1,43 @@
+//#region Import
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+//#endregion
+
+//#region Validation Schema
+const validationSchema = yup
+    .object({
+        name: yup.string().required("Product name is required"),
+        type: yup.string().required("Product type is required")
+    })
+    .required();
+//#endregion
+
+const ProductForm = () => {
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit
+    } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = (data) => console.log(data);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("name")} />
+            {errors.name ? (
+                <span style={{ color: "red" }}>Product name is required</span>
+            ) : null}
+            <input {...register("type")} />
+            {errors.type && (
+                <span style={{ color: "red" }}>Product type is required</span>
+            )}
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default ProductForm;
